88464f6904f650fa963b26ddf0b5102449af2858
ZiDong Fang <424044876@qq.com>
2019-03-15 14:13:37 +0800

    vgg1.0

--- model/vgg.py
+++ model/vgg.py
@@ -7,6 +7,39 @@ class Vgg16(object):
     def __init__(self, vgg_npy_path):
         self.model = np.load(file=vgg_npy_path, encoding='latin1').item()
         print('vgg16.npy loaded')
+        img = tf.placeholder(dtype=[None, 224, 224, 3])
+
+        self.conv1_1 = self.conv2D(self.img, 'conv1_1')
+        self.conv1_2 = self.conv2D(self.conv1_1, 'conv1_2')
+        self.max_pool1 = self.max_pool(self.conv1_2, 'max_pool1')
+
+        self.conv2_1 = self.conv2D(self.max_pool1, 'conv2_1')
+        self.conv2_2 = self.conv2D(self.conv2_1, 'conv2_2')
+        self.max_pool2 = self.max_pool(self.conv2_2, 'max_pool2')
+
+        self.conv3_1 = self.conv2d(self.pool2, 'conv3_1')
+        self.conv3_2 = self.conv2d(self.conv3_1, 'conv3_2')
+        self.conv3_3 = self.conv2d(self.conv3_2, 'conv3_3')
+        self.max_pool3 = self.max_pool(self.conv3_3, 'pool3')
+
+        self.conv4_1 = self.conv2d(self.max_pool3, 'conv4_1')
+        self.conv4_2 = self.conv2d(self.conv4_1, 'conv4_2')
+        self.conv4_3 = self.conv2d(self.conv4_2, 'conv4_3')
+        self.max_pool4 = self.max_pool(self.conv4_3, 'pool4')
+
+        self.conv5_1 = self.conv2d(self.max_pool4, 'conv5_1')
+        self.conv5_2 = self.conv2d(self.conv5_1, 'conv5_2')
+        self.conv5_3 = self.conv2d(self.conv5_2, 'conv5_3')
+        self.max_pool5 = self.max_pool(self.conv5_3, 'max_pool5')
+
+        self.fc6 = self.full_connect(self.max_pool5, 'fc6')
+
+        self.fc7 = self.full_connect(self.fc6, 'fc7')
+
+        self.fc8 = self.full_connect(self.fc7, 'fc8')
+
+        self.softmax = tf.nn.softmax(self.fc8)
+
 
     def get_filter(self, name):
         con_filter = self.model[name][0]
@@ -32,15 +65,15 @@ class Vgg16(object):
         with tf.variable_scope(name_or_scope=name):
             return tf.nn.max_pool(value=input_x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    def convs(self, img):
+    def full_connect(self, input_x, name):
+        with tf.variable_scope(name_or_scope=name):
+            weight = self.get_weight(name)
+            bias = self.get_bias(name)
+            return tf.nn.relu_layer(x=input_x, weights=weight, biases=bias)
 
-        conv1_1 = self.conv2D(img, 'conv1_1')
-        conv1_2 = self.conv2D(self.conv1_1, 'conv1_2')
-        max_pool1 = self.max_pool(self.conv1_2, 'max_pool1')
+    def get_conv_result(self):
+        return self.conv5_3
 
-        conv2_1 = self.conv2D(self.max_pool1, 'conv2_1')
-        conv2_2 = self.conv2D()
-
-
-
+    def get_softmax(self):
+        return self.softmax
 
